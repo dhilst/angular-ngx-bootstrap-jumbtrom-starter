a04c683eeec97f9f90960b4acac86bc8e22209ed
Daniel Hilst Selli <daniel@gmail.com>
2018-03-09 17:37:58 -0300

    Fix server

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const forceSSL = function() {
 
 app.use(morgan('combined'));
 //app.use(forceSSL());
-app.use(express.static(__dirname + 'dist'));
-
-app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/index.html')) });
+app.use(express.static(__dirname + '/dist'));
+app.get('/', function(req,res){ res.sendFile(path.resolve(__dirname, 'dist/index.html')) });
 
 app.listen(process.env.PORT || 8080);
